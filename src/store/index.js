3baf0fe737825c3316030d5d38b40646861a43e9
Sveta <qwe7vbn12@gmail.com>
2021-01-14 01:23:12 +0200

    remover unnecessary module

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import products from "./modules/products";
-import product from "./modules/product";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     products,
-    product,
   },
 });
--- src/store/modules/products.js
+++ src/store/modules/products.js
@@ -7,18 +7,38 @@ export default {
       const productItems = await response.json();
       ctx.commit("updatedProducts", productItems);
     },
+
+    async fetchProduct(ctx, id) {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos/" + id
+      );
+      const product = await response.json();
+      ctx.commit("updatedProduct", product);
+    },
   },
+
   mutations: {
     updatedProducts(state, productItems) {
       state.productItems = productItems;
     },
+
+    updatedProduct(state, product) {
+      state.product = product;
+    },
   },
+
   state: {
     productItems: [],
+    product: [],
   },
+
   getters: {
     products(state) {
       return state.productItems;
+    },
+
+    product(state) {
+      return state.product;
     },
   },
 };
